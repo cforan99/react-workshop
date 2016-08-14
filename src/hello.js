901f78545c566a57477957c8600705cd9a7613e3
Christina Foran <cforan99@gmail.com>
2016-08-14 13:21:50 -0700

    created a hello component with a child component of goodbye

--- src/hello.js
+++ src/hello.js
@@ -1,2 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var Goodbye = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h2>Farewell</h2>
+				<p>Y'all come back now!</p>
+			</div>
+		);
+	}
+
+});
+
+var Hello = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h2>Greetings</h2>
+				<p>Hello beautiful humans and nonsentient bots!</p>
+				<Goodbye />
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<Hello />, document.getElementById('hello'));
