4ee1b4c0c577692ab026017cc95cb3527e3c4bf2
Christina Foran <cforan99@gmail.com>
2016-08-14 15:27:57 -0700

    props and states in parent and child components

--- src/click.js
+++ src/click.js
@@ -1,17 +1,43 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var ClickCounter = React.createClass({
+var ClickCounterCaption = React.createClass({
 	render: function(){
 		return(
-			<button className="btn btn-primary">
-				{ this.props.text }
-			</button>
+			<div className="alert alert-info">
+				You have earned { this.props.points } points!
+			</div>
+		);
+	}
+});
+
+var ClickCounter = React.createClass({
+	handleBtnClick: function(){ 
+		this.setState({clicks: this.state.clicks+5});
+	},
+	getInitialState: function(){
+		return {clicks: this.props.val};
+	},
+	render: function(){
+		var isEven = this.state.clicks % 2 === 0;
+		return(
+			<div>
+				<div className="alert alert-success">
+					{ isEven ? "That's a nice round number!" : "That's really odd!" }
+				</div>
+				<button className="btn btn-primary" onClick={ this.handleBtnClick }>
+					{ this.props.text } &nbsp; 
+					<span className="badge">
+						{this.state.clicks}
+					</span>
+				</button>
+				<ClickCounterCaption points={this.state.clicks} />
+			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<ClickCounter text="Clicks" />, 
+	<ClickCounter text="Points" val={0}/>, 
 	document.getElementById('click-counter')
 );
